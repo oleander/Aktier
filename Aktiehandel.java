ecfaef314340bb9b9b4d818b4c180e676bbd061c
Jesper Josefsson <jesper.josefsson@gmail.com>
2010-11-21 19:43:51 +0100

    påbörjat implementering av parser i aktiehandel

---
+++ Aktiehandel.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+class Aktiehandel {
+  public static void main(String[] args) {
+    ArrayList<String> input = new ArrayList<String>();
+    Scanner s;
+    if (args.length == 0) {
+      s = new Scanner(System.in);
+    } else {
+      s = new Scanner(args[0]);
+    }
+    
+    while (s.hasNextLine()) {
+      input.add(s.nextLine());
+    }
+    System.out.println(input);
+  }
+}
