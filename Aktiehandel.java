08abc5ce732c5b560b7575975effc19723808c11
Jesper Josefsson <jesper.josefsson@gmail.com>
2010-11-21 19:56:10 +0100

    kan läsa filer både via args och via system in

--- Aktiehandel.java
+++ Aktiehandel.java
@@ -1,21 +1,25 @@
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.io.File;
 
 class Aktiehandel {
   public static void main(String[] args) {
     ArrayList<String> input = new ArrayList<String>();
     Scanner s;
+    try {
+      /* Om vi inte har några args så försöker vi läsa från system.in */
+      if (args.length == 0) {
+        s = new Scanner(System.in);
+      } else {
+        s = new Scanner(new File (args[0]));
+      }
     
-    /* Om vi inte har några args så försöker vi läsa från system.in */
-    if (args.length == 0) {
-      s = new Scanner(System.in);
-    } else {
-      s = new Scanner(args[0]);
+      while (s.hasNextLine()) {
+        input.add(s.nextLine());
+      }
+      System.out.println(input);
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
     }
-    
-    while (s.hasNextLine()) {
-      input.add(s.nextLine());
-    }
-    System.out.println(input);
   }
 }
