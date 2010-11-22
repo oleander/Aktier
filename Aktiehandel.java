172b4dfc4faf3d1b1d0805d8a25ac9bdb41847d1
Jesper Josefsson <jesper.josefsson@gmail.com>
2010-11-22 18:33:15 +0100

    skitsaker

--- Aktiehandel.java
+++ Aktiehandel.java
@@ -1,11 +1,22 @@
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 import java.io.File;
 
 class Aktiehandel {
+
   public static void main(String[] args) {
-    ArrayList<String> input = new ArrayList<String>();
+    ArrayList<ArrayList<String>> input = new ArrayList<ArrayList<String>>();
+    final int apa = 10;
+    Pattern p = Pattern.compile("(.+|[\\s.]+)\\s+(KS|K|S|NS)\\s+(\\d+\\s+\\d+|\\d+)");
+    StockExchange se;
+    Matcher m;
     Scanner s;
+    String buffer;
+    String[] parsed;
+    int rowNumber = 1;
+    
     try {
       /* Om vi inte har några args så försöker vi läsa från system.in */
       if (args.length == 0) {
@@ -13,13 +24,30 @@ class Aktiehandel {
       } else {
         s = new Scanner(new File (args[0]));
       }
-    
+      
       while (s.hasNextLine()) {
-        input.add(s.nextLine());
+        ArrayList<String> row = new ArrayList<String>();
+        String a = s.nextLine();
+        System.out.println(a);
+        m = p.matcher(a);
+        if (m.find()) {
+          int groups = m.groupCount();
+          if (groups == 3 || groups == 4)
+            for (int i = 1; i <= groups; i++) {
+              row.add(m.group(i));
+          } else {
+            throw new GeneralException("Your input file has an error on line: " + rowNumber);
+          }
+        }
+        input.add(row);
+        rowNumber++;
       }
+      
       System.out.println(input);
+      
     } catch (Exception e) {
       System.out.println(e.getMessage());
     }
+    
   }
 }
